839136490a4b909ed083ad6fbc098fc561e86f33
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-10-09 22:01:19 +0700

    v161: Attaching to Socket

--- Source/ShooterSam/ShooterSamCharacter.cpp
+++ Source/ShooterSam/ShooterSamCharacter.cpp
@@ -54,10 +54,14 @@ void AShooterSamCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 
+	GetMesh()->HideBoneByName("weapon_r", PBO_None);
+	
 	Gun = GetWorld()->SpawnActor<AGun>(GunClass);
 	if (Gun)
 	{
 		Gun->SetOwner(this);
+		Gun->AttachToComponent(GetMesh(),
+			FAttachmentTransformRules::KeepRelativeTransform, TEXT("WeaponSocket"));
 	}
 }
 
