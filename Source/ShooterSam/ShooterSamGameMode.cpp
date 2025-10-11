74119caeee49b46499dd6a86163406d7b1e503f9
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-10-11 23:14:45 +0700

    v179: Range based for loop

--- Source/ShooterSam/ShooterSamGameMode.cpp
+++ Source/ShooterSam/ShooterSamGameMode.cpp
@@ -20,10 +20,19 @@ void AShooterSamGameMode::BeginPlay()
 
 	TArray<AActor*> ShooterAIActors;
 	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AShooterAI::StaticClass(), ShooterAIActors);
-	for (int32 LoopIndex = 0; LoopIndex < ShooterAIActors.Num(); ++LoopIndex)
-	{
-		AActor* ShooterAIActor = ShooterAIActors[LoopIndex];
+	// for (int32 LoopIndex = 0; LoopIndex < ShooterAIActors.Num(); ++LoopIndex)
+	// {
+	// 	AActor* ShooterAIActor = ShooterAIActors[LoopIndex];
+	//
+	// 	if (AShooterAI* ShooterAI = Cast<AShooterAI>(ShooterAIActor))
+	// 	{
+	// 		ShooterAI->StartBehaviorTree(Player);
+	// 		UE_LOG(LogTemp, Warning, TEXT("%s starting behavior tree"), *ShooterAI->GetActorNameOrLabel());
+	// 	}
+	// }
 
+	for (AActor* ShooterAIActor: ShooterAIActors)
+	{
 		if (AShooterAI* ShooterAI = Cast<AShooterAI>(ShooterAIActor))
 		{
 			ShooterAI->StartBehaviorTree(Player);
