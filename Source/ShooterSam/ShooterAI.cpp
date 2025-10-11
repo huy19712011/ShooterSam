1ea8fe1c1ccb798b3f5e340db01356f7f1768db9
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-10-11 21:57:06 +0700

    v175: VehaviorTrees & Blackboards

--- Source/ShooterSam/ShooterAI.cpp
+++ Source/ShooterSam/ShooterAI.cpp
@@ -9,23 +9,28 @@ void AShooterAI::BeginPlay()
 {
 	Super::BeginPlay();
 
+	if (EnemyAIBehaviorTree)
+	{
+		RunBehaviorTree(EnemyAIBehaviorTree);
+	}
+
 }
 
 void AShooterAI::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (class APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0))
-	{
-		if (LineOfSightTo(PlayerPawn))
-		{
-			SetFocus(PlayerPawn);
-			MoveToActor(PlayerPawn, 200.0f);
-		}
-		else
-		{
-			ClearFocus(EAIFocusPriority::Gameplay);
-			StopMovement();
-		}
-	}
+	// if (class APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0))
+	// {
+	// 	if (LineOfSightTo(PlayerPawn))
+	// 	{
+	// 		SetFocus(PlayerPawn);
+	// 		MoveToActor(PlayerPawn, 200.0f);
+	// 	}
+	// 	else
+	// 	{
+	// 		ClearFocus(EAIFocusPriority::Gameplay);
+	// 		StopMovement();
+	// 	}
+	// }
 }
--- Source/ShooterSam/ShooterAI.h
+++ Source/ShooterSam/ShooterAI.h
@@ -19,4 +19,7 @@ protected:
 
 public:
 	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+	UBehaviorTree* EnemyAIBehaviorTree;
 };
